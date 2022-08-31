ed2e6dda18598eeaa3e8f22e44115effe94b8fb1
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-08-31 14:53:14 -0500

    Added functions and confirmations
    Added functions and confirmations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -14,6 +14,7 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function (){
   password = generatePassword ();
   document.getElementById ("password").Placeholder = password;
+  console.log("Button was clicked")
 });
 
 var generatePassword = function() {
@@ -24,9 +25,45 @@ var generatePassword = function() {
   } else if (userChoice < 8 || userChoice > 128) {
     userChoice = parseInt(prompt("You must choose between 8 and 128"));
   } else {
+    confirmLowerCase = confirm ("Would you like to use lower case characters?");
+    confirmUpperCase = confirm ("Would you like use upper case characters?");
+    confirmSpecialChar = confirm ("Would you like to use special characters?");
+    confirmNumbers = confirm ("Would you like to use numbers?");
+  }
 
+  if (!confirmLowerCase && !confirmUpperCase && !confirmSpecialChar && !confirmNumbers) {
+    choices = alert ("You must choose a criteria");
+  }
+  else if (confirmLowerCase && confirmUpperCase && confirmSpecialChar && confirmNumbers) {
+    choices = lowerCase.concat (upperCase, specialChar, numbers);
+  }
+  else if (confirmLowerCase && confirmUpperCase && confirmSpecialChar) {
+  choices = lowerCase.concat (upperCase, specialChar);
+  }
+  else if (confirmLowerCase && confirmUpperCase) {
+    choices = lowerCase.concat (upperCase);
+  }
+  else if (confirmUpperCase && confirmSpecialChar && confirmNumbers) {
+  choices = upperCase.concat (specialChar, numbers);
+  }
+  else if (confirmUpperCase && confirmSpecialChar) {
+    choices = upperCase.concat (specialChar);
+  }
+  else if (confirmUpperCase && confirmNumbers) {
+    choices = upperCase.concat (numbers);
   }
 }
+
+var genPassword = [];
+for (var i = 0; i < userChoice; i++) {
+  var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+  genPassword.push(pickChoices);
+}
+
+var password = genPassword.join("");
+
+UserInput(password);
+return password;
 
 
 // Get references to the #generate element
